52c221653f4d9135521ceafc9ee989455b33dae0
Sakula <mohittatu2007@gmail.com>
2025-08-13 19:07:58 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# Admin configuration
+ADMIN_IDS = [7521967798]  # Add your admin IDs here
+
+# MongoDB configuration
+MONGODB_URI = "mongodb+srv://dhruvkachhiya75622:c4gb6msfE0jzGtFa@cluster0.6e8wrs6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" 
---
+++ vars.py
@@ -0,0 +1,10 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+from os import environ
+
+API_ID = int(environ.get("API_ID", "24689889"))
+API_HASH = environ.get("API_HASH", "555ff52a3a0b0ef7ad253a6a82ff5c54")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8074079816:AAEOGZFCBSl9OkqwaOS-m8xj6X9uzYfQVQk")
+
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
